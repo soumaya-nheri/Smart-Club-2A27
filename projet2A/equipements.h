3780198a9562f95c453f8f49e63d0630468d4250
aziz-kck <61433292+aziz-kck@users.noreply.github.com>
2021-03-11 15:51:18 +0100

    crud

---
+++ projet2A/equipements.h
@@ -0,0 +1,33 @@
+#ifndef EQUIPEMENTS_H
+#define EQUIPEMENTS_H
+#include"QString"
+#include "QSqlQueryModel"
+class equipements
+{
+public:
+    equipements();
+
+    equipements(int,QString,QString,QString,QString,QString);
+    int getid_equipement();
+    QString getmarque();
+    QString getmodel();
+    QString getprix();
+    QString getetat();
+    QString getdisponibilite();
+    void setid_equipement(int);
+    void setmarque(QString);
+    void setmodel(QString);
+    void setprix(QString);
+    void setetat(QString);
+    void setdisponibilite(QString);
+    bool ajouter();
+    QSqlQueryModel* afficher();
+    bool supprimer(int);
+private:
+    int id_equipement;
+    QString marque,model,prix,etat,disponibilite;
+
+};
+
+#endif // EQUIPEMENTS_H
+
